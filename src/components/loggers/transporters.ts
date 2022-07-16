2f57dfff2cd9c3cdda71040153b65ba9159151e1
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-07-16 09:36:19 -0700

    feat(loggers): create transporters

---
+++ src/components/loggers/transporters.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+
+import winston, { format } from 'winston';
+
+const logDirectory = path.join(__dirname, '..', '..', 'logs');
+const combinedLogFilepath = path.join(logDirectory, 'combined.log');
+const accessLogFilepath = path.join(logDirectory, 'access.log');
+const processingLogFilepath = path.join(logDirectory, 'processing.log');
+const errorLogFilepath = path.join(logDirectory, 'error.log');
+
+const consoleFormat = format.combine(
+  format.timestamp(process.env.NODE_ENV !== 'production' ? { format: 'YYYY-MM-DD HH:mm:ss' } : undefined),
+  format.errors({ stack: true }),
+  format.printf(({ level, message, timestamp, stack }) => `[${timestamp}/${level.toUpperCase()}]: ${stack || message}`),
+);
+
+const fileFormat = format.combine(
+  format.timestamp(),
+  format.json(),
+);
+
+const errorFileFormat = format.combine(
+  format.errors({ stack: true }),
+  fileFormat,
+);
+
+export const ConsoleTransporter = new winston.transports.Console({ format: consoleFormat, level: 'info' });
+export const AccessFileTransporter = new winston.transports.File({ format: fileFormat, filename: accessLogFilepath, level: 'info' });
+export const ProcessingFileTransporter = new winston.transports.File({ format: fileFormat, filename: processingLogFilepath, level: 'info' });
+export const CombinedFileTransporter = new winston.transports.File({ format: fileFormat, filename: combinedLogFilepath });
+export const ErrorFileTransporter = new winston.transports.File({ format: errorFileFormat, filename: errorLogFilepath, level: 'error' });
